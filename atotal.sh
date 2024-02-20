8a5cb53ecdca478e153b9279bad0152a11cc02d7
Carlos Fernandez <cafebo@floridauniversitaria.es>
2024-02-20 09:43:32 +0100

    acabado atotal.sh

---
+++ atotal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+total=0
+
+echo "Elige ciudad:"
+read ciudad
+
+while read -r linea; do
+    sitio=$(echo "$linea" | awk '{print $1}')
+    consumo=$(echo "$linea" | awk '{print $4}')         
+    
+    if [ $ciudad = $sitio ]; then
+        total=$((total+consumo))
+    fi
+done < consumos.txt   
+
+echo "El consumo de $ciudad es: $total."
